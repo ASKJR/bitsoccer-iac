222e317108656f5a3747b329a0d81289901d4586
albertokatojr <albertokatojr@2c160ee3-c6e2-f06a-f35c-53d74dc9531a>
2014-10-12 23:58:44 +0000

    Novo modelo de validação de formulários. Implementado para telas [Login, Cadastro Usuário].

--- trunk/include.php
+++ trunk/include.php
@@ -1,6 +1,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>bitsoccer</title>
 <link rel="stylesheet" href="./css/styles.css" type="text/css" />
+<link rel="stylesheet" href="./css/validationEngine.jquery.css" type="text/css" />
 <link rel="shortcut icon" href="./img/soccer.ico">
 <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
 <script src="./js/jquery-2.1.0.min.js"></script>
@@ -9,3 +10,7 @@
 <script src="./js/jquery.mask.min.js"></script>
 <script src="./js/jquery.validate.min.js"></script>
 <script src="./js/cadastroUser.js"></script>
+<script src="./js/login.js"></script>
+<script src="./js/jquery.validationEngine-pt_BR.js"></script>
+<script src="./js/jquery.validationEngine.js"></script>
+
--- trunk/js/cadastroUser.js
+++ trunk/js/cadastroUser.js
@@ -1,4 +1,5 @@
 $(function(){
+	$("#formCadastroUser").validationEngine();
 	$('.cpf').mask('000.000.000-00', {reverse: true});
 	$('.cep').mask('00000-000');
 	$('.num').mask('0000000000');
