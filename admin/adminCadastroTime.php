3a04b8d9d0a119820d231909cf407d1e865e859f
albertokatojr <albertokatojr@2c160ee3-c6e2-f06a-f35c-53d74dc9531a>
2014-10-15 03:35:38 +0000

    footer atualização

--- admin/adminCadastroTime.php
+++ admin/adminCadastroTime.php
@@ -63,15 +63,7 @@
     	<div class="clear"></div>
     </div>
     <footer>
-		<div class="footer-bottom">
-		  <p>UFPR - Tecnologia em Análise e Desenvolvimento de Sistemas - IAC</p>
-			<p> 
-				Desenvolvedores:
-				Ademilson Santana da Silva, 
-				Alberto Sussumu Kato Junior, 
-				Lucas dos Santos Canestraro.
-			</p>
-		</div>
+		<?php include("../footer.php") ?>
     </footer>
 </div>
 </body>
