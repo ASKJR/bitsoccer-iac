99f31c697aa6e8ee97675676bad98d132f16736d
albertokatojr <albertokatojr@2c160ee3-c6e2-f06a-f35c-53d74dc9531a>
2014-10-16 20:39:14 +0000

    CSS : alteração em admin/adminMenuTop.php ,para ficar selecionado quando for a aba HOME e PESQUISA.

--- admin/adminMenuTop.php
+++ admin/adminMenuTop.php
@@ -1,7 +1,20 @@
+<?php
+	$classAdminHome     ="";
+	$classAdminPesquisa ="";
+	
+	//Ajustando a classe 'css' para o menu do TOP ficar selecionado
+	if($_SERVER["PHP_SELF"] === "/admin/adminHome.php"){
+		$classAdminHome = "start selected";
+	}
+	else if($_SERVER["PHP_SELF"] === "/admin/adminPesquisa.php"){
+		$classAdminPesquisa = "start selected";
+	}
+?>
+
 <div class="width">
 	<ul>
-        		<li><a href="adminHome.php">Home</a></li>
-         	   	<li><a href="adminPesquisa.php">Pesquisa</a></li>
-          	 	<li class="end"><a href="../logout.php">Logout</a></li>
+		<li class="<?=$classAdminHome?>"><a href="adminHome.php">Home</a></li>
+		<li class="<?=$classAdminPesquisa?>"><a href="adminPesquisa.php">Pesquisa</a></li>
+		<li class="end"><a href="../logout.php">Logout</a></li>
 	</ul>
 </div>
